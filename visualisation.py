23f3d259e65ab40a00906fda4a78c81b0f7ec944
Talent Majuru <talentmajuru@gmail.com>
2019-09-17 11:29:55 +0200

    Add plotted script

--- visualisation.py
+++ visualisation.py
@@ -5,3 +5,13 @@ data = np.loadtxt(
 )
 
 import matplotlib.pyplot as plt
+
+fig =  matplotlib.pyplot.figure.(figsize=(3.0,8.0))
+
+axes1 = fig.add_subplot(3, 1, 1)
+axes2 = fig.add_subplot(3, 1, 2)
+axes3 = fig.add_subplot(3, 1, 3)
+
+data = np.loadtxt(fname='inflammation-01.csv', delimiter=',')
+
+fig.tight_layout()
